d47c14e4177343a9dca9755f410362a1d7e0c286
Courtney Buratto <cmburatto@gmail.com>
2020-01-13 15:33:26 -0700

    GET users

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// implement your API here
+const express = require('express')
+const db = require('./data/db.js')
+
+const server = express()
+
+server.get('/', (req, res) => {
+	res.send('Hellowwwww')
+})
+
+//GET
+server.get('/api/users', (req, res) => {
+	db.find()
+		.then(users => {
+			res.status(200).json(users)
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({
+					errorMessage:
+						'There was an error while saving the user to the database'
+				})
+		})
+})
+
+const port = 8000
+server.listen(port, () => {
+	console.log('\n=== API on port 8000 ===\n')
+})
